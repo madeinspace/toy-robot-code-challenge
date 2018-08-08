609a64dc869231b79ad13f1cc147f22527c80d8b
Fabrice Million <fabrice@funkadelik.com>
2018-08-08 12:49:42 +1000

    Added tests expectation

--- tests/app.test.jsx
+++ tests/app.test.jsx
@@ -9,3 +9,28 @@ describe('The Console', () => {
     expect(shallow(<Console />)).toExist();
   });
 });
+
+describe('The Robot', () => {
+
+  it('should ignore commands if hasn\'t been placed on table', () => {
+  });
+
+  it('should ignore commands if moving out of bounds', () => {
+  });
+
+  it('should print position when issued a report command', () => {
+  });
+  
+  it('should position itself on a valid point on the table', () => {
+  });
+
+  it('should move one unit when issued with move command', () => {
+  });
+
+  it('should turn left when issued with left command', () => {
+  });
+
+  it('should turn right when issued with right command', () => {
+  });
+
+});
